4783dab241699e31494dc47c4ba32c43885155a0
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-04-02 12:10:57 -0300

    rotas

---
+++ src/components/formComponents/index.jsx
@@ -0,0 +1,17 @@
+export const Input = () => {
+    return(
+        <input type="text" />
+    )
+}
+
+export const Textarea = () => {
+    return (
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+    )
+}
+
+export const Select = () => {
+    return (
+        <select name="" id=""></select>
+    );
+}
